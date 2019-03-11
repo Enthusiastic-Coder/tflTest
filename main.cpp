39850616ef1fb00c36e10383eac3b9111bee3382
m jebara <jebaramo@gmail.com>
2019-03-11 15:18:19 +0000

    GLES

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setAttribute(Qt::AA_DisableShaderDiskCache);
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseOpenGLES);
+
     QApplication a(argc, argv);
 //    QLibrary ssl;
 //    ssl.setFileName("ssla");
