4c9b4543bea373604f9ca75838b4a016ec6780a5
MOJ_JEB <jebaramo@gmail.com>
2022-01-23 20:41:51 +0000

    OSM tile generation WIP

--- OSMTileGenerator.cpp
+++ OSMTileGenerator.cpp
@@ -34,17 +34,40 @@ void OSMTileGenerator::setUp(Ui::Widget *ui)
 
     connect(ui->pushButtonLoadOSMData, &QPushButton::clicked, this, [this,importOSM] {
 
+        addLog("ImportOSM: BEGIN");
         importOSM(&OSMData::importMotorWay, _ui->lineEditOSMMotorwayPath->text());
         importOSM(&OSMData::importPrimary, _ui->lineEditOSMPrimaryPath->text());
+        importOSM(&OSMData::importSecondary, _ui->lineEditOSMSecondaryPath->text());
+        importOSM(&OSMData::importTertiary, _ui->lineEditOSMTertiaryPath->text());
+        importOSM(&OSMData::importResidential, _ui->lineEditOSMResidentialPath->text());
+        importOSM(&OSMData::importAeroRunway, _ui->lineEditOSMAerorunwayPath->text());
+        importOSM(&OSMData::importAeroWay, _ui->lineEditOSMAerowayPath->text());
+        importOSM(&OSMData::importPedestrian, _ui->lineEditOSMPedestrianPath->text());
+        importOSM(&OSMData::importFootway, _ui->lineEditOSMFootwayPath->text());
+        importOSM(&OSMData::importCycleWay, _ui->lineEditOSMCyclewayPath->text());
+        importOSM(&OSMData::importWater, _ui->lineEditOSMWaterPath->text());
+        addLog("ImportOSM: END");
     });
 
     _osmSettings.push_back({"OSMMotorwayPath", _ui->lineEditOSMMotorwayPath});
     _osmSettings.push_back({"OSMPrimaryPath", _ui->lineEditOSMPrimaryPath});
+    _osmSettings.push_back({"OSMSecondaryPath", _ui->lineEditOSMSecondaryPath});
+    _osmSettings.push_back({"OSMTertiaryPath", _ui->lineEditOSMTertiaryPath});
+    _osmSettings.push_back({"OSMResidentialPath", _ui->lineEditOSMResidentialPath});
+    _osmSettings.push_back({"OSMAeroRunwayPath", _ui->lineEditOSMAerorunwayPath});
+    _osmSettings.push_back({"OSMAerowayPath", _ui->lineEditOSMAerowayPath});
+    _osmSettings.push_back({"OSMPedestrianPath", _ui->lineEditOSMPedestrianPath});
+    _osmSettings.push_back({"OSMPFootwayPath", _ui->lineEditOSMFootwayPath});
+    _osmSettings.push_back({"OSMPCyclePath", _ui->lineEditOSMCyclewayPath});
+    _osmSettings.push_back({"OSMPWaterPath", _ui->lineEditOSMWaterPath});
 
     QSettings s;
 
     for(auto& item : _osmSettings)
         item.edit->setText(s.value(item.name).toString());
+
+    ui->lineEditOSMZoomLevel->setText(s.value("OSMZoomLevel").toString());
+    ui->checkBoxOSMNightTime->setChecked(s.value("OSMNightTime").toBool());
 }
 
 void OSMTileGenerator::unSetup()
@@ -53,19 +76,22 @@ void OSMTileGenerator::unSetup()
 
     for(auto& item : _osmSettings)
         s.setValue(item.name, item.edit->text());
+
+    s.setValue("OSMZoomLevel", _ui->lineEditOSMZoomLevel->text());
+    s.setValue("OSMNightTime", _ui->checkBoxOSMNightTime->isChecked() );
 }
 
 void OSMTileGenerator::generateTiles()
 {
-    addLog("GenerateTiles:");
-    QSize sz(128,128);
+    addLog("GenerateTiles: BEGIN");
+    QSize sz(1024,1024);
 
     std::unique_ptr<TFLOSMRenderer> renderer = std::make_unique<TFLOSMRenderer>(&_data);
 
     renderer->init();
 
     renderer->setPixelLevel(_ui->lineEditOSMZoomLevel->text().toFloat());
-    renderer->setMapNight(false);
+    renderer->setMapNight(_ui->checkBoxOSMNightTime->isChecked());
     renderer->setLocation(GPSLocation(51.4964, -0.300198, 392.0));
     renderer->setSize(sz);
 
@@ -76,6 +102,8 @@ void OSMTileGenerator::generateTiles()
     renderer->paint(p);
 
     image.save("OSM_TILE.png");
+
+    addLog("GenerateTiles: END");
 }
 
 void OSMTileGenerator::addLog(const QString &line)
