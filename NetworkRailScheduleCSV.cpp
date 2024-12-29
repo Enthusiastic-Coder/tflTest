3a39841f96b0d0b1a21eca981f2ad443b3ec37c6
mo <jebaramo@gmail.com>
2024-12-29 16:53:50 +0000

    network rail schedule wip

--- NetworkRailScheduleCSV.cpp
+++ NetworkRailScheduleCSV.cpp
@@ -1,11 +1,21 @@
 #include "NetworkRailScheduleCSV.h"
 
 
-void NetworkRailScheduleCSV::onLine(int lineNo, const QStringList &args) {
+void NetworkRailScheduleCSV::onLine(int lineNo, const QStringList &parts) {
 
-    if( args.length()>=3)
-    {
-        _data[args[0]].firstStanox = args[1];
-        _data[args[0]].lastStanox = args[2];
-    }
+    NetworkRailScheduleDATA schedule;
+
+    schedule.atocCode = parts[0];
+    schedule.serviceCode = parts[1];
+    schedule.firstStanox = parts[2];
+    schedule.lastStanox = parts[3];
+    schedule.firstLocation = parts[4];
+    schedule.lastLocation = parts[5];
+    schedule.departTime = parts[6];
+    schedule.arrivalTime = parts[7];
+    schedule.startDate = QDate::fromString(parts[8], "yyyy-MM-dd");
+    schedule.endDate = QDate::fromString(parts[9], "yyyy-MM-dd");
+    schedule.daysRun = parts[10];
+
+    _data[schedule.atocCode] = schedule;
 }
--- NetworkRailScheduleCSV.h
+++ NetworkRailScheduleCSV.h
@@ -3,17 +3,26 @@
 
 #include "csvfileload.h"
 
+#include <QDate>
 
 struct NetworkRailScheduleDATA {
+    QString atocCode;
+    QString serviceCode;
     QString firstStanox;
     QString lastStanox;
+    QString firstLocation;
+    QString lastLocation;
+    QString departTime;
+    QString arrivalTime;
+    QDate startDate;
+    QDate endDate;
+    QString daysRun;
 };
-
 
 class NetworkRailScheduleCSV : public CSVFileLoad<NetworkRailScheduleDATA>
 {
 public:
-    void onLine(int lineNo, const QStringList &args) override;
+    void onLine(int lineNo, const QStringList &parts) override;
 };
 
 #endif // NETWORKRAILSCHEDULECSV_H
